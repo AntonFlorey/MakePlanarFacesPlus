75d6ff356e1f08a9942c88b656f942d09251397d
AntonFlorey <45452563+AntonFlorey@users.noreply.github.com>
2025-05-23 10:15:42 +0200

    Delete out/build/x64-Debug directory

--- out/build/x64-Debug/extern/eigen-3.4.0/blas/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/Dev/MakePlanarFacesPlus/extern/eigen-3.4.0/blas
-# Build directory: E:/Dev/MakePlanarFacesPlus/out/build/x64-Debug/extern/eigen-3.4.0/blas
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
